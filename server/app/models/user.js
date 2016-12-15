41760925be1f3e5c8bc5615961614902d7403517
hungnph <hungnph@gmail.com>
2016-12-16 00:47:31 +0100

    Authentication

---
+++ server/app/models/user.js
@@ -0,0 +1,81 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+//var bcrypt = require('bcrypt');
+var crypto = require('crypto'); 
+// Thanks to http://blog.matoski.com/articles/jwt-express-node-mongoose/
+ 
+// set up a mongoose model
+var UserSchema = new Schema({
+  name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+  password: {
+      passwordHash: String,
+      salt: String
+        //type: String,
+        //required: true
+    }
+});
+ 
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        user.password.salt = getRandomString(10);
+        user.password.passwordHash = sha512(user.password.passwordHash, user.password.salt);
+        /*
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });*/
+    } else {
+        return next();
+    }
+});
+ 
+UserSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
+/**
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
+
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+var sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+module.exports = mongoose.model('User', UserSchema);
